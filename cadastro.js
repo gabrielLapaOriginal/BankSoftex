41adf4167d521ad534b04155e97346c273dfeaf9
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-07-18 12:58:16 -0300

    .

--- cadastro.js
+++ cadastro.js
@@ -1,92 +1,92 @@
-// const fs = require('fs');
-// const path = './users.json';
-// const readlineSync = require('readline-sync');
+const fs = require('fs');
+const path = './users.json';
+const readlineSync = require('readline-sync');
 
-// const userIds = {};
+const userIds = {};
 
-// //gera id aleatorio
-// function GerarIdAleatorio() {
-//     //caracteres que o id podera conter
-//     const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-//     let id = '';
+//gera id aleatorio
+function GerarIdAleatorio() {
+    //caracteres que o id podera conter
+    const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    let id = '';
 
-//     for (let i = 0; i < 10; i++) {
-//         const randomId = Math.floor(Math.random() * caracteres.length)
-//         id += caracteres[randomId];
-//     }
-//     //Garante que o id nao se repita
-//     if (userIds.hasOwnProperty(id)){
-//         return generateRandomId();
-//     }else {
-//         userIds[id] = true;
-//         return id;
-//     }
-// }
+    for (let i = 0; i < 10; i++) {
+        const randomId = Math.floor(Math.random() * caracteres.length)
+        id += caracteres[randomId];
+    }
+    //Garante que o id nao se repita
+    if (userIds.hasOwnProperty(id)){
+        return generateRandomId();
+    }else {
+        userIds[id] = true;
+        return id;
+    }
+}
 
 
-// //gera numero de cartao de credito aleatorio
-// function generateCreditCardNumber () {
-//     let creditCardNumber = ''
-//     for (let i = 0; i < 16; i++){
-//         creditCardNumber += Math.floor(Math.random() * 10)
-//     }
-//     return creditCardNumber
-// }
+//gera numero de cartao de credito aleatorio
+function generateCreditCardNumber () {
+    let creditCardNumber = ''
+    for (let i = 0; i < 16; i++){
+        creditCardNumber += Math.floor(Math.random() * 10)
+    }
+    return creditCardNumber
+}
 
-// //classe de usuarios
-// class Users {
+//classe de usuarios
+class Users {
     
-//     constructor(nome, cpf){
-//         this.id = GerarIdAleatorio();
-//         this.nome = nome;
-//         this.cartao = generateCreditCardNumber();
-//         this.tipoCartao = "debito";
-//         this.cpf = cpf;
-//         this.limite = 100;
-//         this.saldo = 0;
-//         this.historicoDeDeposito = []
-//     }
-// }
-// //cadastra usuario novo
-// function cadastroUsuario (nome, cpf) {
-//     const newUser = new Users(nome, cpf);
+    constructor(nome, cpf){
+        this.id = GerarIdAleatorio();
+        this.nome = nome;
+        this.cartao = generateCreditCardNumber();
+        this.tipoCartao = "debito";
+        this.cpf = cpf;
+        this.limite = 100;
+        this.saldo = 0;
+        this.historicoDeDeposito = []
+    }
+}
+//cadastra usuario novo
+function cadastroUsuario (nome, cpf) {
+    const newUser = new Users(nome, cpf);
    
-//     fs.readFile(path, 'utf8', (err, data) => {
-//         let users = [];
+    fs.readFile(path, 'utf8', (err, data) => {
+        let users = [];
 
-//         if (!err) {
-//             try {
-//                 users = JSON.parse(data);
-//             } catch (error) {
-//                 console.error('Erro ao parsear JSON:', error);
-//                 return;
-//             }
-//         }
-//             users.push(newUser);
+        if (!err) {
+            try {
+                users = JSON.parse(data);
+            } catch (error) {
+                console.error('Erro ao parsear JSON:', error);
+                return;
+            }
+        }
+            users.push(newUser);
         
 
-//         fs.writeFile(path, JSON.stringify(users, null, 2), 
-//         ((err) => err 
-//         ? console.error(`Erro ao salvar o arquivo: ${err}`) 
-//         : console.log('Usuário cadastrado com sucesso!'))
+        fs.writeFile(path, JSON.stringify(users, null, 2), 
+        ((err) => err 
+        ? console.error(`Erro ao salvar o arquivo: ${err}`) 
+        : console.log('Usuário cadastrado com sucesso!'))
     
-//     );
-//     })
-// }
+    );
+    })
+}
 
 
-// function recuperarUsuarios(){
-//     fs.readFile(path, 'utf8', (err, data) =>{
-//         if (err) {
-//             console.error('Erro ao ler o arquivo:', err);
-//             return
-//         }
-//         let users = JSON.parse(data);
-//         ////debug
-//         //console.log(users); 
-//         return users;
-//     });
-// }
+function recuperarUsuarios(){
+    fs.readFile(path, 'utf8', (err, data) =>{
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return
+        }
+        let users = JSON.parse(data);
+        ////debug
+        //console.log(users); 
+        return users;
+    });
+}
 // //recuperarUsuarios();
 
 
