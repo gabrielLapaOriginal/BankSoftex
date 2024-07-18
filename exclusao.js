ff1765cba79faef276b21f1a6797efc28f9cbecf
Gabriel Henrique Lapa <111655328+gabrielLapaOriginal@users.noreply.github.com>
2024-07-18 15:28:49 -0300

    Adicionando exclusão de usuários.

---
+++ exclusao.js
@@ -0,0 +1,15 @@
+//Precisa do axios e do json-server para funcionar 
+const readlineSync = require("readline-sync");
+//const fs = require("fs");
+const { default: axios } = require("axios");
+
+console.log("Bem vindo a exclusão de usuario\nInfelizmente voce esta nos deixando")
+const idUsuario = readlineSync.question(`Digite o seu id: `)
+
+function exclusao(id) {
+  axios
+    .delete(`http://localhost:3000/users/${id}`)
+    .then(() => console.log("Usuario excluido"));
+}
+
+exclusao(idUsuario);
