ac8f882434a7899af627a37d961e735367aa2b99
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-07-24 15:12:19 -0300

    saque adaptado para axios

--- saque.js
+++ saque.js
@@ -1,32 +1,51 @@
 const readlineSync = require('readline-sync');
+const axios = require('axios');
 const fs = require('fs');
+
+const apiUrl = 'http://localhost:3000/users';
 function saque(userId){
     const valorSaque = Number(readlineSync.question(`Digite o valor que voce deseja sacar: `))
-    const userData = JSON.parse(fs.readFileSync('users.json'));
-    usuarioOrigem =  userData.find(usuario => usuario.id == userId)
+    axios.get(apiUrl)
+    .then(response => {
+    const usuarios = response.data;
+
+
+    usuarioOrigem =  usuarios.find(usuario => usuario.id == userId)
 
     if(!usuarioOrigem || usuarioOrigem.saldo < valorSaque){
         console.error("Usuario ou valor invalidos!!")
     }else{
-        usuarioOrigem.saldo -= valorSaque;
-        console.log(`verifique o caixa eletronico para seu saque no valor de ${valorSaque}\n
-            O valor atual da conta e de ${usuarioOrigem.saldo}`)
-        fs.writeFileSync('users.json', JSON.stringify(userData));
+        // usuarioOrigem.saldo -= valorSaque;
+        const updatedUser = {saldo: usuarioOrigem.saldo -= valorSaque}
+        axios.patch(`${apiUrl}/${usuarioOrigem.id}`, updatedUser)
+        .then(() => {
+            console.log(`verifique o caixa eletronico para seu saque no valor de R$ ${valorSaque} \nValor atual da conta e de R$ ${usuarioOrigem.saldo}`)
+  })
+  .catch(error => {
+    console.error('Erro ao atualizar o usuário:', error);
+  });
+
     }
+})
 }
+
 function deposito(userId){
     const valorDeposito = Number(readlineSync.question(`Digite o valor que voce deseja depositar: `))
-    const userData = JSON.parse(fs.readFileSync('users.json'));
-    usuarioOrigem =  userData.find(usuario => usuario.id == userId)
+    axios.get(apiUrl)
+    .then(response => {
+    const usuarios = response.data;
+    usuarioOrigem =  usuarios.find(usuario => usuario.id == userId)
 
     if(!usuarioOrigem){
         console.error("Usuario invalido!!")
     }else{
-        usuarioOrigem.saldo += valorDeposito;
-        console.log(`Voce fez um deposito no valor de ${valorDeposito}\n
-            O valor atual da conta e de ${usuarioOrigem.saldo}`)
-        fs.writeFileSync('users.json', JSON.stringify(userData));
+        const updatedUser = {saldo: usuarioOrigem.saldo += valorDeposito}
+        axios.patch(`${apiUrl}/${usuarioOrigem.id}`, updatedUser)
+        .then(() => {
+            console.log(`Voce fez um deposito no valor de ${valorDeposito}\nO valor atual da conta e de ${usuarioOrigem.saldo}`)
+        })
     }
+})
 }
 
 module.exports = {
