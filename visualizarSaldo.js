2de65aa6aa73c7e62f3f927b0ee42614fe70dcca
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-07-24 14:49:10 -0300

    Visualizar saldo com axios

--- visualizarSaldo.js
+++ visualizarSaldo.js
@@ -1,10 +1,21 @@
-const readlineSync = require('readline-sync');
-const fs = require('fs');
+const axios = require('axios');
+const apiUrl = 'http://localhost:3000/users';
+
 
 function visSaldo(userId){
-    const userData = JSON.parse(fs.readFileSync('users.json'));
-    usuarioOrigem =  userData.find(usuario => usuario.id == userId)
-    console.log(`Seu saldo atual e de ${usuarioOrigem.saldo}`)
+
+    axios.get(apiUrl)
+    .then(response => {
+    const usuarios = response.data;
+  
+    const usuarioOrigem = usuarios.find((usuario) => usuario.id === userId);
+    console.log(`Seu saldo atual e de R$ ${usuarioOrigem.saldo}`)
+   
+  })
+
+    // const userData = JSON.parse(fs.readFileSync('users.json'));
+    // usuarioOrigem =  userData.find(usuario => usuario.id == userId)
+    // console.log(`Seu saldo atual e de ${usuarioOrigem.saldo}`)
 
     //aqui quebramos o código depois
 }
