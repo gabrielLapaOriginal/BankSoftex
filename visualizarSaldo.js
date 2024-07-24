7af898f343cbf97c4a65142337a4c6f899fcb746
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-07-24 14:50:00 -0300

    Apagando comentarios

--- visualizarSaldo.js
+++ visualizarSaldo.js
@@ -10,14 +10,7 @@ function visSaldo(userId){
   
     const usuarioOrigem = usuarios.find((usuario) => usuario.id === userId);
     console.log(`Seu saldo atual e de R$ ${usuarioOrigem.saldo}`)
-   
   })
-
-    // const userData = JSON.parse(fs.readFileSync('users.json'));
-    // usuarioOrigem =  userData.find(usuario => usuario.id == userId)
-    // console.log(`Seu saldo atual e de ${usuarioOrigem.saldo}`)
-
-    //aqui quebramos o código depois
 }
 visSaldo('xG31t2YhD9')
 module.exports = {
